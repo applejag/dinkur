3ec7928306d6a68633e84bfe2a00750ed859e7f8
Kalle Fagerberg <kalle.jillheden@gmail.com>
2021-12-30 11:25:43 +0100

    Moved dinkurdb/client.go into dinkurdb.go

--- pkg/dinkurdb/dinkurdb.go
+++ pkg/dinkurdb/dinkurdb.go
@@ -25,7 +25,10 @@ import (
 	"math"
 	"strconv"
 
+	"github.com/dinkur/dinkur/pkg/dinkur"
+	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var (
@@ -42,4 +45,81 @@ func nilNotFoundError(err error) error {
 		return nil
 	}
 	return err
+}
+
+type Options struct {
+	SkipMigrateOnConnect bool
+}
+
+func NewClient(dsn string, opt Options) dinkur.Client {
+	return &client{Options: opt, sqliteDsn: dsn}
+}
+
+type client struct {
+	Options
+	sqliteDsn     string
+	db            *gorm.DB
+	prevMigStatus dinkur.MigrationStatus
+}
+
+func (c *client) Connect() error {
+	if c.db != nil {
+		return ErrAlreadyConnected
+	}
+	var err error
+	c.db, err = gorm.Open(sqlite.Open(c.sqliteDsn), &gorm.Config{
+		Logger: logger.Discard,
+		//Logger: logger.New(log.New(colorable.NewColorableStdout(), "\r\n", log.LstdFlags), logger.Config{
+		//	SlowThreshold:             200 * time.Millisecond,
+		//	LogLevel:                  logger.Info,
+		//	IgnoreRecordNotFoundError: false,
+		//	Colorful:                  true,
+		//}),
+	})
+	if err != nil {
+		return err
+	}
+	sqlDB, err := c.db.DB()
+	if err != nil {
+		return err
+	}
+	sqlDB.SetMaxOpenConns(1)
+	if !c.SkipMigrateOnConnect {
+		return c.Migrate()
+	}
+	return nil
+}
+
+func (c *client) Ping() error {
+	if c.db == nil {
+		return ErrNotConnected
+	}
+	sql, err := c.db.DB()
+	if err != nil {
+		return err
+	}
+	return sql.Ping()
+}
+
+func (c *client) Close() error {
+	if c.db == nil {
+		return ErrNotConnected
+	}
+	sql, err := c.db.DB()
+	if err != nil {
+		return err
+	}
+	if err := sql.Close(); err != nil {
+		return err
+	}
+	c.db = nil
+	return nil
+}
+
+func (c *client) transaction(f func(tx *client) error) error {
+	return c.db.Transaction(func(tx *gorm.DB) error {
+		newClient := *c
+		newClient.db = tx
+		return f(&newClient)
+	})
 }
