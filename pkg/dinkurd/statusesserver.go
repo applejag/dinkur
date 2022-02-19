814fb2a1dcce6ace490d5195864f14119b9ac694
Kalle Fagerberg <kalle.f8@protonmail.com>
2022-02-19 22:34:16 +0100

    Added dinkurserver status impl

--- pkg/dinkurd/statusesserver.go
+++ pkg/dinkurd/statusesserver.go
@@ -19,4 +19,67 @@
 
 package dinkurd
 
-// TODO: implement this
+import (
+	"context"
+
+	dinkurapiv1 "github.com/dinkur/dinkur/api/dinkurapi/v1"
+	"github.com/dinkur/dinkur/pkg/dinkur"
+	"github.com/dinkur/dinkur/pkg/fromgrpc"
+	"github.com/dinkur/dinkur/pkg/togrpc"
+)
+
+func (d *daemon) StreamStatus(req *dinkurapiv1.StreamStatusRequest, stream dinkurapiv1.Statuses_StreamStatusServer) error {
+	if err := d.assertConnected(); err != nil {
+		return convError(err)
+	}
+	if req == nil {
+		return convError(ErrRequestIsNil)
+	}
+	ctx, cancel := context.WithCancel(stream.Context())
+	defer cancel()
+	ch, err := d.client.StreamStatus(ctx)
+	if err != nil {
+		return convError(err)
+	}
+	for ev := range ch {
+		if err := stream.Send(&dinkurapiv1.StreamStatusResponse{
+			Status: togrpc.Status(ev.Status),
+		}); err != nil {
+			return convError(err)
+		}
+	}
+	return nil
+}
+
+func (d *daemon) SetStatus(ctx context.Context, req *dinkurapiv1.SetStatusRequest) (*dinkurapiv1.SetStatusResponse, error) {
+	if err := d.assertConnected(); err != nil {
+		return nil, convError(err)
+	}
+	if req == nil {
+		return nil, convError(ErrRequestIsNil)
+	}
+	err := d.client.SetStatus(ctx, dinkur.EditStatus{
+		AFKSince:  fromgrpc.TimePtr(req.AfkSince),
+		BackSince: fromgrpc.TimePtr(req.BackSince),
+	})
+	if err != nil {
+		return nil, convError(err)
+	}
+	return &dinkurapiv1.SetStatusResponse{}, nil
+}
+
+func (d *daemon) GetStatus(ctx context.Context, req *dinkurapiv1.GetStatusRequest) (*dinkurapiv1.GetStatusResponse, error) {
+	if err := d.assertConnected(); err != nil {
+		return nil, convError(err)
+	}
+	if req == nil {
+		return nil, convError(ErrRequestIsNil)
+	}
+	status, err := d.client.GetStatus(ctx)
+	if err != nil {
+		return nil, convError(err)
+	}
+	return &dinkurapiv1.GetStatusResponse{
+		Status: togrpc.Status(status),
+	}, nil
+}
